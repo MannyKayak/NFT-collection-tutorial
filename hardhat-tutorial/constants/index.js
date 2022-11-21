3ddd7af8332be31c7c6ad5611e38bddb307478a3
VirtualRedy <gustavo_alfonso@hotmail.it>
2022-11-21 14:41:54 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x4072d7ff59f3f2b63b9017c7C7cD9836278D67ff";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
